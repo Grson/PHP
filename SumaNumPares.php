fc1bd578d09012631f39d1e1c3cf397ce026cf30
Gersón Alvarado <91508918+Grson@users.noreply.github.com>
2022-01-06 15:57:13 -0600

    Add files via upload

---
+++ SumaNumPares.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$total = 0;
+$number = 0;
+ 
+while ($number <= 10)
+{
+    if ($number % 2 == 0)
+    {
+        $total = $total + $number;
+    }
+    $number++;
+}
+ 
+echo $total;
+ 
+?>

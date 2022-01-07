fefdf709744e9f60b8145f0c4b36475011531c83
Gersón Alvarado <91508918+Grson@users.noreply.github.com>
2022-01-06 20:09:11 -0600

    Add files via upload

---
+++ PositivoNegativoCero.php
@@ -0,0 +1,29 @@
+<center><h2>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+
+	<?php
+
+
+$Num = -5; 
+if ($Num > 0) 
+{
+    echo $Num . ". Es un número positivo";
+} else if ($Num < 0) 
+{
+    echo $Num . ". Es un número negativo ";
+} else if ($Num == 0) 
+{
+    echo ". El número es cero";
+} else {
+    echo ". Debe escribir un número entero";
+}
+
+
+?>
+
+</h2></center>

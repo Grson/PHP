5df7dee78b3c8ac1abdf7fc2f138befc4c3189e5
Gersón Alvarado <91508918+Grson@users.noreply.github.com>
2022-01-06 19:52:49 -0600

    Add files via upload

---
+++ AreaRectangulo.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function area_rectangulo($Largo = 10, $Ancho = 5) 
+{
+    $AreaT = $Largo * $Ancho;
+    echo "El área del rectángulo con largo " . $Largo . " y ancho " . $Ancho . " es " . $AreaT  . " (m)2 ";
+ }
+area_rectangulo(); 
+
+
+?>

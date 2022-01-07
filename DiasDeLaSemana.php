202716a2317334e29d7fb86fef5d943d5e2d6791
Gersón Alvarado <91508918+Grson@users.noreply.github.com>
2022-01-06 19:36:31 -0600

    Add files via upload

---
+++ DiasDeLaSemana.php
@@ -0,0 +1,29 @@
+<?php
+$dia = "2";
+
+switch ($dia) {
+    case "1":
+        echo "¡¡¡Es Lunes!!!";
+        break;
+    case "2":
+        echo "¡¡¡Es Martes!!!";
+        break;
+    case "3":
+        echo "¡¡¡Es Miércoles!!!";
+        break;
+  case "4":
+        echo "¡¡¡Es Jueves!!!";
+        break;
+    case "5":
+        echo "¡¡¡Es Viernes!!!";
+        break;
+    case "6":
+        echo "¡¡¡Es Sabado!!!";
+        break;
+  case "7":
+        echo "¡¡¡Es Domingo!!!";
+        break;
+    default:
+        echo "Introduzca un número del 1 al 7";
+}
+?>
